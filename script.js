924bfa39419a989d4186fe62f21c4164ed5013cc
FarzanaPrianka <farzana.prianka@gmail.com>
2020-02-14 16:18:44 -0500

    add namespace object

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log("hey halley and prianka");
+//create app movieArc to hold all methods
+
+const movieArc = {};
+
+movieArc.apiKey = "a106247c";
+
+//collect user input
+
+movieArc.collectInput = function() {};
+
+//Make ajax request with user input data
+
+movieArc.getInfo = function() {};
+
+//display the data on the page
+
+movieArc.displayInfo = function() {};
+
+//----------------------------------------------
+//collect user input for the second*** time
+
+movieArc.collectSecondInput = function() {};
+
+//Make second ajax request with user input data for the second*** time
+
+movieArc.getSecondInfo = function() {};
+
+//display the data on the page
+
+movieArc.displaySecondInfo = function() {};
+
+//-----------------------------------------------
+
+//start app
+
+movieArc.init = function() {};
+
+//doc ready
+
+$(function() {
+  movieArc.init();
+});
